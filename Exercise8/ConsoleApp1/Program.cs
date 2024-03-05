fad89b64c1f4fb51cb11f4aff8d0264abbe26816
TheHiddenOne <145466606+TheHiddenOne-Mges@users.noreply.github.com>
2024-03-05 09:50:09 -0500

    loops are fun, Completed for loops

---
+++ Exercise8/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please set a Value");
+            int  value = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <=value; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("how many asterisks do you need");
+            int asterrisksNeeded = int.Parse(Console.ReadLine());
+            for (int g = 1; g <= asterrisksNeeded; g++)
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
